e73cc1fe25942f07879da660c84071ae5694c963
akiko-satou9 <tekumaku.akkochan.k@gmail.com>
2021-05-02 03:37:16 +0000

    add column

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -3,22 +3,26 @@
 @section('content')
 
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
-
     
-    {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
+    <div class="row">
+        <div class="col-6">
+    
+            {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
         
-        <div class="form-group">
-            {!! Form::label('title', 'タイトル:') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
-        </div>
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
         
-        <div class="form-group">
-            {!! Form::label('content', 'メッセージ:') !!}
-            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            {!! Form::close() !!}
         </div>
-
-        {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
-
-    {!! Form::close() !!}
+    </div>
     
 @endsection

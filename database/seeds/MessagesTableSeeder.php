acd5f2e1993c645c2f9073aa966b58a7c1abc64b
akiko-satou9 <tekumaku.akkochan.k@gmail.com>
2021-05-02 06:19:33 +0000

    add seeder

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -11,12 +11,18 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 100; $i++) {
-            DB::table('messages')->insert([
-                'title' => 'test title ' . $i,
-                'content' => 'test content ' . $i
-            ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 1',
+            'content' => 'test content 1'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 2',
+            'content' => 'test content 2'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 3',
+            'content' => 'test content 3'
+        ]);
         
-        }
     }
 }
